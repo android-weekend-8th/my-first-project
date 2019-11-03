64f865f6cb51bbc3b4365873a04c4b3203df141a
Rathana Voy <rathanavoy07@gmai.com>
2019-11-03 11:30:08 +0700

    merge with lisin

--- DateUtil.java
+++ DateUtil.java
@@ -1,13 +1,11 @@
-<<<<<<< HEAD
+
 class DateUtil{
 
     public void show(){
         System.out.println("create show method");
-=======
-class DateUtil {
+    }
 
     public String getCurrentDate(){
         return null;
->>>>>>> origin/lisin
     }
 }
